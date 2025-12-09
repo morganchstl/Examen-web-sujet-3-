117cc0a89e31aa8eafe8270373528470f8ca83a6
morganchstl <mochastel@eleve.st-charles.eu>
2025-12-09 15:43:30 +0000

    java script/ajout barre de text

---
+++ script.js
@@ -0,0 +1,11 @@
+const juste = document.querySelector("réponse");
+    if ("réponse"){
+        const juste = "bonjour"
+ 
+ answer.addEventListener('submit', (event) => {
+            event.preventDefault();
+       const reponsevrai = document.querySelector('valider');
+        if (reponsevrai) {
+            reponsevrai.addtListener('click', () => {
+                document.body.classList('true');
+    })}})};
